b4306dacc1cfd132960518cac5eb03f2512d2793
Faraaz Khan <faraaz@rationalizeit.us>
2020-03-24 16:22:33 -0500

    some updates

--- main.tf
+++ main.tf
@@ -12,23 +12,19 @@ provider "aws" {
 ##############################################################
 data "aws_vpc" "default" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["idr-cloud-dev"]
   }
 }
 
 data "aws_subnet_ids" "all" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["idr-cloud-dev-data-a", "idr-cloud-dev-data-b"]
   }
   vpc_id = data.aws_vpc.default.id
 }
 
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "IDR-Cloud-SG"
-}
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
@@ -43,27 +39,49 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+resource "aws_security_group" "haproxy" {
+  name        = "haproxy"
+  description = "SG for HA proxy description"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "All traffic from self"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    self        = true
+    #security_groups = [aws_security_group.haproxy.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 ######
 # Launch configuration and autoscaling group
 ######
 module "example_asg" {
-  source = "terraform-aws-modules/autoscaling/aws"
-  version = "~> 3.0"
+  source               = "terraform-aws-modules/autoscaling/aws"
+  version              = "~> 3.0"
   iam_instance_profile = "s3-access-haproxy"
 
-  name = "haproxy"
+  name     = "haproxy"
   key_name = "key-cms-idr-cloud"
 
   # Launch configuration
   #
   # launch_configuration = "my-existing-launch-configuration" # Use the existing launch configuration
   # create_lc = false # disables creation of launch configuration
-  lc_name = "haproxy-lc"
-  user_data = templatefile("${path.module}/files/userdata.tpl", { s3_bucket = "${var.s3_bucket}"})
+  lc_name   = "haproxy-lc"
+  user_data = templatefile("${path.module}/files/userdata.tpl", { s3_bucket = "${var.s3_bucket}" })
 
   image_id        = data.aws_ami.amazon_linux.id
   instance_type   = "t2.micro"
-  security_groups = [data.aws_security_group.default.id]
+  security_groups = [aws_security_group.haproxy.id]
   # load_balancers  = [module.elb.this_elb_id]
 
   ebs_block_device = [
@@ -88,7 +106,7 @@ module "example_asg" {
   health_check_type         = "EC2"
   min_size                  = 1
   max_size                  = 2
-  desired_capacity          = 0
+  desired_capacity          = 1
   wait_for_capacity_timeout = 0
 
   tags = [
@@ -110,10 +128,10 @@ module "example_asg" {
 ######
 ############
 resource "aws_lb" "idr-nlb-haproxy" {
-  name               = "idr-nlb-haproxy"
-  internal           = true
-  load_balancer_type = "network"
-  subnets            = data.aws_subnet_ids.all.ids
+  name                       = "idr-nlb-haproxy"
+  internal                   = true
+  load_balancer_type         = "network"
+  subnets                    = data.aws_subnet_ids.all.ids
   enable_deletion_protection = false
   tags = {
     Environment = "Dev"
@@ -121,12 +139,12 @@ resource "aws_lb" "idr-nlb-haproxy" {
 }
 
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = "${aws_lb.idr-nlb-haproxy.arn}"
+  load_balancer_arn = aws_lb.idr-nlb-haproxy.arn
   port              = "443"
   protocol          = "TCP"
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.idr-haproxy-nlbtg.arn}"
+    target_group_arn = aws_lb_target_group.idr-haproxy-nlbtg.arn
   }
 }
 
@@ -155,7 +173,7 @@ resource "aws_vpc_endpoint" "s3" {
 
 data "aws_route_table" "rt1" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["idr-cloud-dev-data-a"]
   }
   vpc_id = data.aws_vpc.default.id
@@ -163,7 +181,7 @@ data "aws_route_table" "rt1" {
 
 data "aws_route_table" "rt2" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["idr-cloud-dev-data-b"]
   }
   vpc_id = data.aws_vpc.default.id
